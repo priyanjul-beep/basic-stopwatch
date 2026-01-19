a4edc606f0d37ffa96ba1a50004eaeac52c30ade
Priyanjul Sonkar <priyanjul@appsforbharat.com>
2026-01-19 23:47:26 +0530

    update stopwatch

--- src/components/Stopwatch.jsx
+++ src/components/Stopwatch.jsx
@@ -1,23 +1,20 @@
+import { useState, useRef } from "react";
 
-import {useState,useRef} from "react";
-
-
-function Stopwatch(){
-  const [time,setTime] = useState(0);
-
-  const [isRunning,setIsRunning] = useState(false);
+function Stopwatch() {
+  const [time, setTime] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
 
   const intervalRef = useRef(null);
 
   const startTimer = () => {
-    if(!isRunning){
-        setIsRunning(true);
-        intervalRef.current = setInterval(() => {
-            setTime((prevTime) => prevTime + 1);
-        },1000);
+    if (!isRunning) {
+      setIsRunning(true);
+
+      intervalRef.current = setInterval(() => {
+        setTime((prevTime) => prevTime + 10);
+      }, 10);
     }
   };
-
 
   const pauseTimer = () => {
     clearInterval(intervalRef.current);
@@ -30,10 +27,21 @@ function Stopwatch(){
     setTime(0);
   };
 
+  const milliseconds = Math.floor((time % 1000) / 10);
+  const seconds = Math.floor((time / 1000) % 60);
+  const minutes = Math.floor((time / (1000 * 60)) % 60);
+  const hours = Math.floor(time / (1000 * 60 * 60));
+
   return (
     <div>
       <h1>Stopwatch</h1>
-      <h2>{time}</h2>
+
+      <h2>
+        {hours.toString().padStart(2, "0")}:
+        {minutes.toString().padStart(2, "0")}:
+        {seconds.toString().padStart(2, "0")}:
+        {milliseconds.toString().padStart(2, "0")}
+      </h2>
 
       {!isRunning ? (
         <button onClick={startTimer}>
